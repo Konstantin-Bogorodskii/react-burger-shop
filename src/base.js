936d718d9c6df8ee7e799dd55842898da9f41a5e
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-09-22 15:52:34 +0300

    added auth and deploy project

--- src/base.js
+++ src/base.js
@@ -2,11 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase/app';
 require('firebase/database');
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyBdY7370EhqVAolA2SoqDF24kaaUwqG9pw',
-
-  authDomain: 'burger-shop-react-6f88f.firebaseapp.com',
-
-  databaseURL: 'https://burger-shop-react-6f88f-default-rtdb.firebaseio.com',
+  apiKey: 'AIzaSyBpmEVPcQBE_cvNwO_tSzu1bExGXrXk3iI',
+  authDomain: 'burger-shop-8bfe7.firebaseapp.com',
+  databaseURL: 'https://burger-shop-8bfe7-default-rtdb.firebaseio.com',
 });
 
 const base = Rebase.createClass(firebaseApp.database());
